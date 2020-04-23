50b8af4d81cf9ee2bca72b324a7045df9af0cd44
Jayee <jayee.jiayi.fu@gmail.com>
2020-04-23 14:28:27 +0800

    babel 和 typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
-let hello:string = 'Hello TypeScriot'
+let hello:string = "hello world"
 
-// ForkTsCheckerWebpackPlugin可以检查出这里的错误，
-// awesome-typescript-loader 的 CheckerPlugin 则不能检查出错误
+class A {
+    a:number = 1
+}
+
+let {x,y,...z} = {x:2,y:4,a:5,c:6}
+let n = {x,y,...z}
+
+// Babel 本省无法检查类型错误，需要安装 TypeScript 来检查
 // hello = 1
-document.querySelectorAll('.app')[0].innerHTML = hello
+
+
+// 注意：有四种语法在 Babel 中是无法编译的
+// 1.命名空间
+// namespace N{
+//     export const n = 1
+// }
+
+// 2.常量枚举
+// const enum E{A}
+
+// 3.尖括号的类型断言
+// let s = <A>{}
+// s.a = 1
+
+// 4.默认导出
+// export = A
